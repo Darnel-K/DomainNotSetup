6c6dd9f811824a9795d2131c7682ece0d2e2e7ef
Darnel-K <darnel.kumar91@gmail.com>
2017-12-13 21:51:25 +0000

    -

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 }
             ?>
             </h2>
-            <p><?php echo date("d/m/Y h:i:s A e");?></p>
+            <p><?php echo date("d/m/Y h:i:s A T");?></p>
         <hr>
         <p><?php echo $_SERVER['REQUEST_TIME']; ?> &bull; Your IP <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
     </div>
