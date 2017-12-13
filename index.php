d56a0ead8f7a6c832a2fc170dc47b660b435a2e2
Darnel-K <darnel.kumar91@gmail.com>
2017-12-13 22:44:51 +0000

    -

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
             <?php 
                 if ($_SERVER['HTTP_HOST'] == gethostbyname($_SERVER['HTTP_HOST'])) {
                     echo "<h2 id='Error'>Direct IP access is forbidden</h2>";
-                    echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error page because you tried to access this web server directly using it's IP address.</p></section>";
+                    echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error message because you tried to access this web server directly using it's IP address.</p></section>";
                 } else {
                     echo "<h2 id='Error'>Host '" . $_SERVER['HTTP_HOST'] . "' does not exist on this server</h2>";
-                    echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error page because you tried to access a host that does not exist on this server.</p></section>";
+                    echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error message because you tried to access a host that does not exist on this server.</p></section>";
                 }
             ?>
         <p id="info"><?php echo date("d/m/Y h:i:s A T"); ?> &bull; <?php echo date("e"); ?> &bull; Your IP <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
