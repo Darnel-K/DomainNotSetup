e42ae6beb6e8420a60f4504efc54c3962a8fec34
Darnel Kumar <darnel.kumar91@gmail.com>
2018-12-06 12:12:20 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
         $date = date("d/m/Y h:i:s A T");
         $region = date("e");
         $userIP = $_SERVER['REMOTE_ADDR'];
-        $admin = $_SERVER['SERVER_ADMIN'];
         $signature = $_SERVER['SERVER_SIGNATURE'];
     ?>
     <div id="Container">
@@ -20,12 +19,12 @@
                 <?php
                     if ($_SERVER['HTTP_HOST'] == gethostbyname($_SERVER['HTTP_HOST'])) {
                         http_response_code(403);
-                        $admin = $admin . "?subject=" . rawurlencode("Direct IP access is forbidden");
+                        $admin = $_SERVER['SERVER_ADMIN'] . "?subject=" . rawurlencode("Direct IP access is forbidden");
                         echo "<h2 id='Error'>Direct IP access is forbidden</h2>";
                         echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error message because you tried to access this web server directly using it's IP address.<br><br>If you think this is a server error, please contact the server admin using the link below.</p></section>";
                     } else {
                         http_response_code(404);
-                        $admin = $admin . "?subject=" . rawurlencode("Host '" . $_SERVER['HTTP_HOST'] . "' is non-existent");
+                        $admin = $_SERVER['SERVER_ADMIN'] . "?subject=" . rawurlencode("Host '" . $_SERVER['HTTP_HOST'] . "' is non-existent");
                         echo "<h2 id='Error'>Host '" . $_SERVER['HTTP_HOST'] . "' is non-existent</h2>";
                         echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error message because you tried to access a host that does not exist on this server.<br><br>If you think this is a server error, please contact the server admin using the link below.</p></section>";
                     }
